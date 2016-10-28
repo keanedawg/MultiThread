1c414f5a7677b7f03368aff577e2b9afd8b62a15
keanedawg <runningwind23@gmail.com>
2016-10-28 06:08:54 -0600

    Tried including AsyncTask

--- app/src/main/java/com/example/multithread/MainActivity.java
+++ app/src/main/java/com/example/multithread/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.multithread;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,15 +9,21 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.security.Policy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +33,58 @@ public class MainActivity extends AppCompatActivity {
 
     Context myContext = this;
     ArrayAdapter<String> adapter;
+
+
+
+    class Read extends AsyncTask<Void, Integer, Void> {
+
+        ArrayAdapter<String> adpt;
+        File file;
+        Scanner scan;
+
+        Read(ArrayAdapter<String> a, File f) {
+            adpt = a;
+            file = f;
+            try {
+                scan = new Scanner(file);
+            }
+            catch (IOException e) {
+                Toast.makeText(MainActivity.this, "Error reading file!", Toast.LENGTH_SHORT).show();
+                // I should do something... just not sure what.
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Integer i = 0;
+         //   Toast.makeText(MainActivity.this, "Error reading file!", Toast.LENGTH_SHORT).show();
+            while (scan.hasNext()) {
+                String j = scan.next();
+                adpt.add(j);
+                i++;
+                publishProgress(i);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            adpt.notifyDataSetChanged();
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            scan.close();
+        }
+
+
+    }
 
 
 
@@ -51,13 +110,13 @@ public class MainActivity extends AppCompatActivity {
                         FIO.write(myVal.getBytes());
                         Thread.sleep(250);
                     }
-                    FIO.close();
+                   FIO.close();
                 }
                 catch (IOException i) {
-
+                    Toast.makeText(MainActivity.this, "Error reading file!", Toast.LENGTH_SHORT).show();
                 }
                 catch (InterruptedException e) {
-
+                    Toast.makeText(MainActivity.this, "Interruption", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -81,24 +140,24 @@ public class MainActivity extends AppCompatActivity {
 
                     ListView lv = (ListView) findViewById(R.id.lv);
                     lv.setAdapter(adapter);
-                    adapter.setNotifyOnChange(true);
 
 
+                    Read r = new Read(adapter, file);
+                    r.execute();
 
+
+/*
                     for (int i = 0; i < 10; i++) {
                         if(scan.hasNext()) {
                             Thread.sleep(250);
                             adapter.add(scan.next());
                             //adapter.notifyDataSetChanged(); // thought experiment
                         }
-                    }
+                    }*/
 
 
                 }
                 catch (IOException e) {
-
-                }
-                catch (InterruptedException e) {
 
                 }
             }
